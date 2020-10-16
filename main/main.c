b81f6d3bce236cf469726aed625137cfb5ed50d1
Rodrigo HS <rodrigohs.srs@hotmail.com>
2020-10-16 11:56:29 -0300

    First modifications on state machine

--- main/main.c
+++ main/main.c
@@ -17,16 +17,16 @@
 #include "driver/gpio.h"
 
 /* Private definitions and enumerations */
-#define MASTERTX_SLAVERX_MSG_SIZE	2
+#define SLAVERX_MASTERTX_MSG_SIZE	2
 #define NUM_OF_SLAVES				32
 #define UART_RX_BUFFER_SIZE			256
 
 typedef enum {
-	TDMA_ST_MASTERTX_SLAVERX = 0,
-	TDMA_ST_MASTERRX_SLAVETX,
-	TDMA_ST_CHECK_NEXT_SLAVE,
-	TDMA_ST_EXEC_USER_PROGRAM,
-} tdma_state_t;
+	SLAVE_ST_SLAVERX_MASTERTX = 0,
+	SLAVE_ST_CHECK_SLAVE_ADDR,
+	SLAVE_ST_PARSE_MASTER_MSG,
+	SLAVE_ST_SLAVETX_MASTERRX,
+} slave_state_t;
 
 typedef struct {
 	bool erase_address_cmd;
@@ -36,22 +36,24 @@ typedef struct {
 } slave_ctrl_t;
 
 /* Private variables */
-uint8_t mastertx_msg[MASTERTX_SLAVERX_MSG_SIZE];
-uint8_t masterrx_msg;
+uint8_t slavetx_msg;
+uint8_t slaverx_msg[SLAVERX_MASTERTX_MSG_SIZE];
 
-tdma_state_t tdma_state = TDMA_ST_MASTERTX_SLAVERX;
+slave_state_t slave_state = SLAVE_ST_SLAVERX_MASTERTX;
 slave_ctrl_t slave_ctrl[NUM_OF_SLAVES] = {0};
-bool initial_scan_done = false;
 bool new_message_arrived = false;
 bool set_new_slave_address_needed = false;
-uint8_t new_slave_address = 0;
+uint8_t current_slave_address = 0; // TODO Use static RAM attribute
 
 /* Private functions */
 void lora_rx_done_callback(uint8_t* buffer_rx, int pac_size)
 {
-	memset(&masterrx_msg, 0, sizeof(masterrx_msg));
-	memcpy(&masterrx_msg, buffer_rx, sizeof(masterrx_msg));
-	new_message_arrived = true;
+	/* Slave must receive packets only from the Master */
+	if ( pac_size == 2 ){
+		memset(&slaverx_msg, 0, sizeof(slaverx_msg));
+		memcpy(&slaverx_msg, buffer_rx, sizeof(slaverx_msg));
+		new_message_arrived = true;
+	}
 }
 
 uint8_t get_next_available_slave (uint8_t current_slave_address)
@@ -174,7 +176,19 @@ void parse_set_new_address_cmd_message (uint8_t new_slave_address, uint8_t* mess
 	*message = msb_message;
 }
 
-void parse_received_message_from_slave (uint8_t slave_address, uint8_t message)
+uint8_t get_slave_address_from_master_message (uint8_t* message)
+{
+	uint16_t raw_message = 0;
+
+	raw_message |= ( (uint16_t)(message[0]) );
+	raw_message |= ( ( (uint16_t)(message[1]) ) << 8 );
+
+	uint8_t address_bits = (uint8_t)( (raw_message >> 2) & 0x001F );
+
+	return address_bits;
+}
+
+void parse_received_message_from_master (uint8_t slave_address, uint8_t message)
 {
 	uint8_t number_of_ones = 0;
 	for (int i = 0; i < 5; i++) {
@@ -204,10 +218,6 @@ void execute_user_program ()
 
 void uart_interface_task(void *p)
 {
-	while (!initial_scan_done) {
-		vTaskDelay(1);
-	}
-
     uint8_t data[ UART_RX_BUFFER_SIZE + 1 ] = {0};
     int uart_rxBytes = 0;
     uint32_t last_slave_info_print_tick = xTaskGetTickCount();
@@ -315,110 +325,44 @@ void uart_interface_task(void *p)
     }
 }
 
-void tdma_task(void *p)
+void slave_task(void *p)
 {
 	uint32_t tick_timeout = 0;
-	uint8_t current_slave_adress = 0;
-
-	/* Initial scan for detects available slaves */
-	uint8_t slaves_founds = 0;
-	printf(LOG_COLOR(LOG_COLOR_CYAN));
-	printf("\nInitial scan for detecting available slaves\n");
-	for ( uint8_t i = 0 ; i < NUM_OF_SLAVES ; i++ ) {
-		new_message_arrived = false;
-		memset(mastertx_msg, 0, sizeof(mastertx_msg));
-		parse_io_cmd_message(i, mastertx_msg);
-		lora_send_packet(mastertx_msg, sizeof(mastertx_msg));
-		lora_receive();
-		tick_timeout = xTaskGetTickCount();
-		printf("Initial scan slave %u", (unsigned int)i);
-		while (xTaskGetTickCount() - tick_timeout < pdMS_TO_TICKS(200)) {
-			if (new_message_arrived) {
-				slave_ctrl[i].available_slave = true;
-				printf(" - available");
-				slaves_founds++;
-				break;
-			}
-			vTaskDelay(1);
-		}
-		printf("\n");
-	}
-	printf("Initial scan found %u slaves\n", (unsigned int)slaves_founds);
-	printf(LOG_RESET_COLOR);
-	initial_scan_done = true;
 
 	for(;;) {
-		switch (tdma_state) {
-			case TDMA_ST_MASTERTX_SLAVERX:
-				memset(mastertx_msg, 0, sizeof(mastertx_msg));
-				new_message_arrived = false;
-
-				/* Prepare the message to be transmitted to the slave N */
-				if (current_slave_adress != 0) {
-					/* Non-zero address slave */
-					if (is_erase_slave_address_needed(current_slave_adress)) {
-						parse_erase_address_cmd_message(current_slave_adress, mastertx_msg);
-						slave_ctrl[0].available_slave = true;
-					}
-					else {
-						parse_io_cmd_message(current_slave_adress, mastertx_msg);
-					}
-				}
-				else {
-					/* Zero address slave */
-					if (is_set_new_slave_address_needed()) {
-						parse_set_new_address_cmd_message(new_slave_address, mastertx_msg);
-						slave_ctrl[0].available_slave = false;
-						slave_ctrl[new_slave_address].available_slave = true;
-					}
-					else {
-						parse_io_cmd_message(current_slave_adress, mastertx_msg);
-					}
-				}
-
-				lora_send_packet(mastertx_msg, sizeof(mastertx_msg));
-
-				lora_receive();
-				tick_timeout = xTaskGetTickCount();
-				tdma_state = TDMA_ST_MASTERRX_SLAVETX;
-				break;
-
-			case TDMA_ST_MASTERRX_SLAVETX:
-				if (xTaskGetTickCount() - tick_timeout >= pdMS_TO_TICKS(100)) {
-					/* Time to listen slave N transmission has expired
-					 * Pass to the next slave available on the network */
-					tdma_state = TDMA_ST_CHECK_NEXT_SLAVE;
-				}
-				else {
-					/* Verify if the master has received a message
-					 * If true, update the inputs table and then
-					 * pass to the next slave available on the network */
-					if (new_message_arrived) {
-						if ( current_slave_adress != 0) {
-							parse_received_message_from_slave(current_slave_adress, masterrx_msg);
-						}
-						else {
-							slave_ctrl[0].available_slave = true;
-						}
-						tdma_state = TDMA_ST_CHECK_NEXT_SLAVE;
-					}
+		switch (slave_state) {
+			case SLAVE_ST_SLAVERX_MASTERTX:
+				/* If arrived new message, check if is to this slave */
+				if (new_message_arrived) {
+					new_message_arrived = false;
+					slave_state = SLAVE_ST_CHECK_SLAVE_ADDR;
 				}
 				break;
 
-			case TDMA_ST_CHECK_NEXT_SLAVE:
+			case SLAVE_ST_CHECK_SLAVE_ADDR:
+				if ( get_slave_address_from_master_message(slaverx_msg) != current_slave_address ) {
+					lora_receive();
+					slave_state = SLAVE_ST_SLAVERX_MASTERTX;
+				}
+				else {
+					slave_state = SLAVE_ST_PARSE_MASTER_MSG;
+				}
+				break;
+
+			case SLAVE_ST_PARSE_MASTER_MSG:
 				/* Verify if the scan is complete */
 				if (get_next_available_slave(current_slave_adress) <= current_slave_adress) {
-					tdma_state = TDMA_ST_EXEC_USER_PROGRAM;
+					slave_state = SLAVE_ST_EXEC_USER_PROGRAM;
 				}
 				else {
-					tdma_state = TDMA_ST_MASTERTX_SLAVERX;
+					slave_state = SLAVE_ST_MASTERTX_SLAVERX;
 				}
 				current_slave_adress = get_next_available_slave(current_slave_adress);
 				break;
 
-			case TDMA_ST_EXEC_USER_PROGRAM:
+			case SLAVE_ST_SLAVETX_MASTERRX:
 				execute_user_program();
-				tdma_state = TDMA_ST_MASTERTX_SLAVERX;
+				slave_state = SLAVE_ST_MASTERTX_SLAVERX;
 				break;
 
 			default:
@@ -445,6 +389,7 @@ void app_main()
 	lora_set_spreading_factor(7);
 	lora_enable_crc();
 	lora_onReceive(&lora_rx_done_callback);
+	lora_receive();
 
 	/* UART interface initialization */
     uart_config_t uart_config = {
@@ -468,9 +413,9 @@ void app_main()
 			NULL,
 			1);
 
-    /* Time division multiple access task initialization */
-	xTaskCreatePinnedToCore(&tdma_task,
-			"tdma",
+    /* Slave task initialization */
+	xTaskCreatePinnedToCore(&slave_task,
+			"slave",
 			8192,
 			NULL,
 			5,
